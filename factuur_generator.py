e333f1936f599c7fbaae265554958257c7cd3643
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-04-15 13:42:04 +0200

    update

--- factuur_generator.py
+++ factuur_generator.py
@@ -2,10 +2,10 @@ from pypdf import *
 from fpdf import *
 from reportlab.pdfgen.canvas import Canvas
 
-naam = ("Naam van het bedrijf: ")
-datum = ("Wat is de datum: ")
-factuurnummer = ("Wat is het factuurnummer: ")
-factuur_adres = ("Wat is het factuur adres: ")
+naam = input("Naam van het bedrijf: ")
+datum = input("Wat is de datum: ")
+factuurnummer = input("Wat is het factuurnummer: ")
+factuur_adres = input("Wat is het factuur adres: ")
 logo_afbeelding = 'afbeeldingen/factuur_enzo_logo.png'
 
 # Create instance of FPDF class
@@ -25,10 +25,16 @@ pdf.image(logo_afbeelding, x=155, y=-6, w=50)  # pas x, y, en w aan volgens je b
 
 pdf.ln(20)
 # Add a paragraph
-pdf.cell(200, 6, txt = f"Naam: {naam}", ln = True, align = 'L')
-pdf.cell(200, 6, txt = f"Datum: {datum}", ln = True, align = 'L')
+pdf.cell(200, 6, txt = f"{naam}", ln = True, align = 'L')
+pdf.cell(200, 6, txt = f"{datum}", ln = True, align = 'L')
 pdf.cell(200, 6, txt = f"Factuurnummer: {factuurnummer}", ln = True, align = 'L')
 pdf.cell(200, 6, txt = f"Factuuradres: {factuur_adres}", ln = True, align = 'L')
+
+pdf.set_y(40)
+pdf.cell(190, 6, txt = f"Factuur Enzo", ln = True, align = 'R')
+pdf.cell(190, 6, txt = f"+31 6123456789", ln = True, align = 'R')
+pdf.cell(190, 6, txt = f"factuurenzo@help.com", ln = True, align = 'R')
+pdf.cell(190, 6, txt = f"factuurenzo.nl", ln = True, align = 'R')
 
 # Save the PDF
 pdf_output = "my_pdf_document.pdf"
