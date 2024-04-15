50e2d7554fa23005e6c63edc3e5a4ae99e2aa976
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-04-15 13:21:20 +0200

    factuur generator update

--- factuur_generator.py
+++ factuur_generator.py
@@ -14,23 +14,21 @@ pdf = FPDF()
 # Add a page
 pdf.add_page()
 
+pdf.set_font("Arial", size=25) 
+pdf.cell(30, 10, txt = "Factuur", ln = True, align = 'C')
+
 # Set font for the title
 pdf.set_font("Arial", size = 12)
 
-# Title
-pdf.cell(200, 10, txt = "Factuur", ln = True, align = 'C')
-
 # Adding logo image
-pdf.image(logo_afbeelding, x=130, y=8, w=50)  # pas x, y, en w aan volgens je behoeften
+pdf.image(logo_afbeelding, x=155, y=-6, w=50)  # pas x, y, en w aan volgens je behoeften
 
+pdf.ln(20)
 # Add a paragraph
-pdf.cell(200, 10, txt = f"Naam: {naam}", ln = True, align = 'L')
-pdf.cell(200, 10, txt = f"Datum: {datum}", ln = True, align = 'L')
-pdf.cell(200, 10, txt = f"Factuurnummer: {factuurnummer}", ln = True, align = 'L')
-pdf.cell(200, 10, txt = f"Factuuradres: {factuur_adres}", ln = True, align = 'L')
-
-# Add another paragraph
-pdf.cell(200, 10, txt = "This is another paragraph in the PDF.", ln = True, align = 'L')
+pdf.cell(200, 6, txt = f"Naam: {naam}", ln = True, align = 'L')
+pdf.cell(200, 6, txt = f"Datum: {datum}", ln = True, align = 'L')
+pdf.cell(200, 6, txt = f"Factuurnummer: {factuurnummer}", ln = True, align = 'L')
+pdf.cell(200, 6, txt = f"Factuuradres: {factuur_adres}", ln = True, align = 'L')
 
 # Save the PDF
 pdf_output = "my_pdf_document.pdf"
