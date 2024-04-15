ebdc89cf62cfbcbe89fd8561847b87aaaff7d382
Bent-Bon <143714061+Bent-Bon@users.noreply.github.com>
2024-04-15 12:05:42 +0200

    fpdf testing

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 from pypdf import *
+from fpdf import *
 from reportlab.pdfgen.canvas import Canvas
 
-naam = input("Naam van het bedrijf: ")
-datum = ("Wat is de datum: ")
-factuurnummer = ("Wat is het factuurnummer: ")
-factuur_adres = ("Wat is het factuur adres: ")
+# naam = input("Naam van het bedrijf: ")
+# datum = ("Wat is de datum: ")
+# factuurnummer = ("Wat is het factuurnummer: ")
+# factuur_adres = ("Wat is het factuur adres: ")
 
 # canvas = Canvas("test.pdf")
 # canvas.drawString(72, 72, "Hello, World!")s
+
+pdf = FPDF
