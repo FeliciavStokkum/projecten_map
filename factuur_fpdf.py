e83335af39268873c6ca1073d232c5cfa863f059
Bent-Bon <143714061+Bent-Bon@users.noreply.github.com>
2024-04-15 13:20:59 +0200

    small update

--- factuur_fpdf.py
+++ factuur_fpdf.py
@@ -1,9 +1,14 @@
 from fpdf import FPDF
 
-naam = input("Naam van het bedrijf: ")
-datum = input("Wat is de datum: ")
-factuurnummer = input("Wat is het factuurnummer: ")
-factuur_adres = input("Wat is het factuur adres: ")
+# naam = input("Naam van het bedrijf: ")
+# datum = input("Wat is de datum: ")
+# factuurnummer = input("Wat is het factuurnummer: ")
+# factuur_adres = input("Wat is het factuur adres: ")
+naam = ("pookie <3")
+datum = ("vandaag")
+factuurnummer = ("69")
+factuur_adres = ("berkel")
+logo_afbeelding = 'afbeeldingen/factuur_enzo_logo.png'
 
 # Create instance of FPDF class
 pdf = FPDF()
@@ -11,13 +16,22 @@ pdf = FPDF()
 # Add page
 pdf.add_page()
 
+# title size
+pdf.set_font("Arial", size = 25)
+pdf.ln(10)
+pdf.cell(200, 10, txt = "INVOICE", ln = True, align = 'L')
+
 # font for title
 pdf.set_font("Arial", size = 12)
 
-# Title
-pdf.cell(200, 10, txt = "My PDF Document", ln = True, align = 'C')
+# Adding logo image
+pdf.image(logo_afbeelding, x=150, y=8, w=50)  # pas x, y, en w aan volgens je behoeften
+
+# Add space
+pdf.ln(10)
 
 # Add a paragraph
+
 pdf.cell(200, 10, txt = f"name: {naam}", ln = True, align = 'L')
 
 pdf.cell(200, 3, txt = f"date: {datum}", ln = True, align = 'L')
