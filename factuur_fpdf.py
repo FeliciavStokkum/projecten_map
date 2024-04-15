0fc805c22b8efab703b452f911579e83c9256da8
Bent-Bon <143714061+Bent-Bon@users.noreply.github.com>
2024-04-15 14:03:20 +0200

    update table

--- factuur_fpdf.py
+++ factuur_fpdf.py
@@ -1,64 +1,51 @@
 from fpdf import FPDF
 
-# naam = input("Naam van het bedrijf: ")
-# datum = input("Wat is de datum: ")
-# factuurnummer = input("Wat is het factuurnummer: ")
-# factuur_adres = input("Wat is het factuur adres: ")
-naam = ("pookie <3")
-datum = ("vandaag")
-factuurnummer = ("69")
-factuur_adres = ("berkel")
+naam = input("Naam van het bedrijf: ")
+datum = input("Wat is de datum: ")
+factuurnummer = input("Wat is het factuurnummer: ")
+factuur_adres = input("Wat is het factuur adres: ")
 logo_afbeelding = 'afbeeldingen/factuur_enzo_logo.png'
-prijs = 0
-prijs_totaal = 0
+data = [['Beschrijving', 'Aantal', 'Eenheid', 'Tarief', 'BTW%', 'BTW', 'Totaal'],
+        ['Werk', 'xxx', 'uur', '€xx', 'xx%', '€xx', '€xx'],
+        ['Producten', 'xxx', 'stuk', '€xx', 'xx%', '€xx', '€xx']]
 
 # Create instance of FPDF class
 pdf = FPDF()
 
-# Add page
+# Add a page
 pdf.add_page()
 
-# title size
-pdf.set_font("Arial", size = 25)
-pdf.ln(10)
-pdf.cell(200, 10, txt = "INVOICE", ln = True, align = 'L')
+pdf.set_font("Arial", size=25) 
+pdf.cell(30, 10, txt = "Factuur", ln = True, align = 'C')
 
-# font for title
+# Set font for the title
 pdf.set_font("Arial", size = 12)
 
 # Adding logo image
 pdf.image(logo_afbeelding, x=155, y=-6, w=50)  # pas x, y, en w aan volgens je behoeften
 
-# Add space
-pdf.ln(10)
-
+pdf.ln(20)
 # Add a paragraph
+pdf.cell(200, 6, txt = f"{naam}", ln = True, align = 'L')
+pdf.cell(200, 6, txt = f"{datum}", ln = True, align = 'L')
+pdf.cell(200, 6, txt = f"Factuurnummer: {factuurnummer}", ln = True, align = 'L')
+pdf.cell(200, 6, txt = f"Factuuradres: {factuur_adres}", ln = True, align = 'L')
 
-pdf.cell(200, 3, txt = f"date: {datum}", ln = True, align = 'L')
-
-pdf.cell(195, 10, txt = "Factuur Enzo", ln = True, align = 'R')
-
-pdf.cell(195, 10, txt = "Tell: +316 85 12 34 56", ln = True, align = 'R')
-
-pdf.cell(200, 10, txt = f"name: {naam}", ln = True, align = 'L')
-
-pdf.cell(200, 10, txt = f"document nummber: {factuurnummer}", ln = True, align = 'L')
-
-pdf.cell(200, 3, txt = f"document adress: {factuur_adres}", ln = True, align = 'L')
+pdf.set_y(40)
+pdf.cell(190, 6, txt = f"Factuur Enzo", ln = True, align = 'R')
+pdf.cell(190, 6, txt = f"+31 6123456789", ln = True, align = 'R')
+pdf.cell(190, 6, txt = f"factuurenzo@help.com", ln = True, align = 'R')
+pdf.cell(190, 6, txt = f"factuurenzo.nl", ln = True, align = 'R')
 
 pdf.ln(10)
 
-data = [['aantal', 'omschrijving', 'prijs per eenheid', 'regeltotaal'],
-        ['8 uur', 'consulancy', '€' + prijs, '€' + prijs_totaal],
-        ['1', 'omderhoud boekhoudsysteem', '€' + prijs, '€' + prijs_totaal],
-        ['product', 'flipover classic', '€' + prijs, '€' + prijs_totaal]]
-
+# Adding table
 for row in data:
     for item in row:
         # Add cell with padding
         pdf.cell(40, 10, txt=item, border=1)
     pdf.ln()
 
-# Save PDF
+# Save the PDF
 pdf_output = "my_pdf_document.pdf"
 pdf.output(pdf_output)
