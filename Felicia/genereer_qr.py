3a2b59f832215d684c8bb8843a0a5462139a899e
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-04-16 22:56:19 +0200

    updates

---
+++ Felicia/genereer_qr.py
@@ -0,0 +1,22 @@
+import qrcode
+
+# Data to be encoded in the QR code
+data = "https://www.example.com"
+
+# Create QR code instance
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+
+# Add data to the QR code
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the QR code as an image file
+img.save("qr_code.png")
